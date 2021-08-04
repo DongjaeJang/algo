a5018dba0fac035f6c71e85570c042455db680ca
DongjaeJang <jangdj001234@gmail.com>
2021-08-04 21:41:34 +0900

    add swea_d1_2046

---
+++ SWEA/D1/2046/Solution.java
@@ -0,0 +1,13 @@
+package swea.d1.d1_2046;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < sc.nextInt(); i++) {
+			System.out.print('#');
+		}
+	}
+}
