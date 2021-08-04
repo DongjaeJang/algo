ac0bf3a36335cbbeb7ab2bf672294a36da4abfa5
DongjaeJang <jangdj001234@gmail.com>
2021-08-04 21:46:02 +0900

    add swea_d1_2043

---
+++ SWEA/D1/2043/Solution.java
@@ -0,0 +1,11 @@
+package swea.d1.d1_2043;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int P = sc.nextInt(), K = sc.nextInt();
+		System.out.println(P-K+1);
+	}
+}
