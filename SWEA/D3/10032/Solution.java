f078ae719f4eb4809cdb907ab870a3a03e48e1f1
DongjaeJang <jangdj001234@gmail.com>
2021-08-13 20:45:11 +0900

    add swea_d3_10032

---
+++ SWEA/D3/10032/Solution.java
@@ -0,0 +1,25 @@
+package swea.d3.d3_10032;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder answer = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			answer.append("#").append(tc).append(" ").append(N % M == 0 || M % N == 0 ? 0 : 1).append("\n");
+		}
+		System.out.println(answer);
+	}
+}
